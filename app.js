bc527a0d517138f951c4363280ca6293a5cc69d3
Theo Chang <theochang@theos-air.lan>
2021-08-04 15:23:08 -0700

    Ingredients

--- app.js
+++ app.js
@@ -1,48 +1,60 @@
-const alchohalName = document.querySelector('h1')
-const drinkDescription = document.querySelector('.drinking_description')
-const drinkIngredient = document.querySelector('.cocktailIng')
-const drinkImage = document.querySelector('img')
-
+const alchohalName = document.querySelector("h1");
+const drinkDescription = document.querySelector(".drinking_description");
+const drinkIngredient = document.querySelector("#cocktailIng");
+const drinkImage = document.querySelector("img");
 
 const getCocktailImg = async () => {
   try {
-    const cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-    const response = await axios.get(cocktailURL)
+    const cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+    const response = await axios.get(cocktailURL);
 
-    let stringIng = []
-    let obj = response.data.drinks[0]
+    let stringIngredient = [];
+    let obj = response.data.drinks[0];
     for (let x in obj) {
-      if (x.includes("strMeasure")) {
-        stringIng.push(obj[x]);
+      if (x.includes("strIngredient")) {
+        stringIngredient.push(obj[x]);
       }
     }
-    // console.log(stringIng)
-
-    const measurements = stringIng.filter(function (a) {
+    console.log(obj);
+    const measurements = stringIngredient.filter(function (a) {
       return a != null;
-    })
+    });
 
     for (let i = 0; i < measurements.length; i++) {
       // console.log(measurements)
+      const row = document.createElement("div");
+      row.classList.add("Ingredient-Row");
 
-      drinkIngredient.append(measurements)
-      drinkIngredient.innerText = measurements[i]
+      const ingredientList = document.createElement('ul')
+      const ingredient = document.createElement("li");
 
+      // const ingredient = document.createElement("div");
+      row.appendChild(ingredientList)
+      ingredientList.appendChild(ingredient);
+      ingredient.innerText = measurements[i];
+
+      drinkIngredient.appendChild(row);
+      ingredient.classList.add("Ingredient-ID");
     }
-    console.log(measurements)
 
+    // for (let v in obj) {
+    //   if(v.includes(""))
+    // }
 
-    // const cocktail = response.data.drinks[0]
+    // console.log(measurements);
+
+    const cocktail = response.data.drinks[0]
     // console.log(cocktail)
-    // appendImage(cocktail)
-    // return cocktail
+    appendImage(cocktail)
 
+    return measurements;
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
-}
+};
 
-getCocktailImg()
+getCocktailImg();
+
 
 function appendImage(ImgURL) {
   drinkImage.src = ImgURL.strDrinkThumb
@@ -56,5 +68,10 @@ function appendImage(ImgURL) {
 
 function getCocktailName(drinkName) {
   alchohalName.textContent = drinkName.strDrink
+
+
 }
 
+function getCocktailMeasurment() {
+
+}
