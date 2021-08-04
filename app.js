bdf48a6123ec75bac95bb6c816bc7f7916794651
Theo Chang <theochang@theos-air.lan>
2021-08-03 22:20:13 -0700

    api text

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const drinkName = document.querySelector('.drink_name')
+const alchohalName = document.querySelector('h1')
 const drinkDescription = document.querySelector('.drinking_description')
 const drinkIngredient = document.querySelector('drinking_ingredient')
 const drinkImage = document.querySelector('img')
@@ -8,9 +8,11 @@ const getCocktailImg = async () => {
   try {
     const cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
     const response = await axios.get(cocktailURL)
-    const image = response.data.drinks[0].strDrinkThumb
+    const cocktail = response.data.drinks[0]
+    console.log(cocktail)
+    appendImage(cocktail)
+    return cocktail
 
-    appendImage(image)
   } catch (e) {
     console.error(e)
   }
@@ -19,8 +21,41 @@ const getCocktailImg = async () => {
 getCocktailImg()
 
 function appendImage(ImgURL) {
+  drinkImage.src = ImgURL.strDrinkThumb
 
-  drinkImage.src = ImgURL
+  const drinkName = ImgURL
 
+  getCocktailName(drinkName)
+  return ImgURL
 }
+
+
+function getCocktailName(drinkName) {
+  alchohalName.textContent = drinkName.strDrink
+}
+
+// const getCocktailName = async (drinkName) => {
+
+//   try {
+//     const cocktailURLName = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+//     const response = await axios.get(cocktailURLName)
+
+//     console.log(response)
+
+//     // const drinkingName = response.data.drinks[0]
+
+//     // appendName(drinkingName)
+
+//   } catch (err) {
+//     console.error(err)
+//   }
+
+// }
+
+// function appendName(nameURL) {
+//   alchohalName.textContent = nameURL.strDrink
+// }
+
+// `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`
+
 
